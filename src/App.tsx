46e3eb6f6a00d3b3191d7183d35b7d8ef459caa5
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-03-11 17:04:38 +0530

    commit: initial commit project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  
+  return (
+    <>
+     <h1 className='text-red-700 text-center text-4xl mt-10 font-bold underline'>hello</h1>
+    </>
+  )
+}
+
